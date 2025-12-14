a1e17ec1785475f654983fc271cdc46a7e7dd89c
Nawahovvp <nawahovvp@gmail.com>
2025-12-14 14:48:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — PartsGo v15.8.8 (29 พ.ย. 2568) — แก้ comma + เพิ่มความเสถียร
-const VERSION = 'v17.2.9';
+const VERSION = 'v17.3.0';
 const CACHE = `partgo-${VERSION}`;
 
 const SHELL = [
